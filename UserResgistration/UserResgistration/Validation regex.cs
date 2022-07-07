4fd0cefea2aa84558c30731ac6741f60b69d9bc7
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-08 00:49:53 +0530

    UC1 solution added

---
+++ UserResgistration/UserResgistration/Validation regex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserResgistration
+{
+    internal class Validation_regex
+    {
+        public void Checkfirstname(string firstname)
+        {
+            string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(firstname, Firstnamepattern))
+                Console.WriteLine("First Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+    }
+}
