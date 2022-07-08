2c010c588127b48f12ff2dff46fea5618d25ad1f
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-09 00:57:05 +0530

    UC4- Mobile Format Solution

--- UserResgistration/UserResgistration/Program.cs
+++ UserResgistration/UserResgistration/Program.cs
@@ -9,27 +9,26 @@ namespace UserResgistration
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Validate last name \n3. Validate EmailID \n4. Exit");
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Validate last name \n3. Validate EmailID \n4. Validate Mobile No.  \n5. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
-                    //case 1:
-                    //    Console.WriteLine("*First letter should be capital.");
-                    //    Console.WriteLine("*length should be Minimum three characters.");
-                    //    Console.WriteLine("Enter First name :");
-                    //    string fname = Console.ReadLine();
-
-                    //    Validation_regex Checkpattern = new Validation_regex();
-                    //    Checkpattern.Checkfirstname(fname);
-                    //    break;
-                    //case 2:
-                    //    Console.WriteLine("*Last letter should be capital.");
-                    //    Console.WriteLine("*length should be Minimum three characters.");
-                    //    Console.WriteLine("Enter Last name :");
-                    //    string lname = Console.ReadLine();
-                    //    Validation_regex lnamepattern = new Validation_regex();
-                    //    lnamepattern.Checklastname(lname);
-                    //    break;
+                    case 1:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter First name :");
+                        string fname = Console.ReadLine();
+                        Validation_regex Checkpattern = new Validation_regex();
+                        Checkpattern.Checkfirstname(fname);
+                        break;
+                    case 2:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter Last name :");
+                        string lname = Console.ReadLine();
+                        Validation_regex lnamepattern = new Validation_regex();
+                        lnamepattern.Checklastname(lname);
+                        break;
                     case 3:
                         Console.WriteLine("Enter Email id :");
                         string email = Console.ReadLine();
@@ -37,6 +36,12 @@ namespace UserResgistration
                         emailpattern.Checkemail(email);
                         break;
                     case 4:
+                        Console.WriteLine("Enter Mobile No. :");
+                        string mobilenum = Console.ReadLine();
+                        Validation_regex mobilenumpattern = new Validation_regex();
+                        mobilenumpattern.Checkmobilenum(mobilenum);
+                        break;
+                    case 5:
                         flag = false;
                         break;
                     default:
--- UserResgistration/UserResgistration/Validation regex.cs
+++ UserResgistration/UserResgistration/Validation regex.cs
@@ -9,22 +9,22 @@ namespace UserResgistration
 {
     public class Validation_regex
     {
-        //public void Checkfirstname(string firstname)
-        //{
-        //    string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
-        //    if (Regex.IsMatch(firstname, Firstnamepattern))
-        //        Console.WriteLine("First Name is valid");
-        //    else
-        //        Console.WriteLine("Invalid name!!");
-        //}
-        //public void Checklastname(string Lastname)
-        //{
-        //    string lastnamepattern = "^[A-Z][a-z]{3,10}$";
-        //    if (Regex.IsMatch(Lastname, lastnamepattern))
-        //        Console.WriteLine("Last Name is valid");
-        //    else
-        //        Console.WriteLine("Invalid name!!");
-        //}
+        public void Checkfirstname(string firstname)
+        {
+            string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(firstname, Firstnamepattern))
+                Console.WriteLine("First Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+        public void Checklastname(string Lastname)
+        {
+            string lastnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(Lastname, lastnamepattern))
+                Console.WriteLine("Last Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
         public void Checkemail(string Emailid)
         {
             string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
@@ -33,6 +33,13 @@ namespace UserResgistration
             else
                 Console.WriteLine("Invalid name!!");
         }
-
+        public void Checkmobilenum(string Mobilenum)
+        {
+            string mobilenumpattern = "^[1,9]{2}-[6-9]{1}[0-9]{9}";
+            if (Regex.IsMatch(Mobilenum, mobilenumpattern))
+                Console.WriteLine("Mobile number is valid");
+            else
+                Console.WriteLine("Invalid mobile number!!");
+        }
     }
 }
