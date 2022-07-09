2467bedc61fa39a817547479720d8245e0c800eb
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-09 18:28:00 +0530

    At Least One Number in Password

--- UserResgistration/UserResgistration/Validation regex.cs
+++ UserResgistration/UserResgistration/Validation regex.cs
@@ -43,7 +43,9 @@ namespace UserResgistration
         }
         public void Checkpassward(string Passward)
         {
-            string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            //string passwardpattern = "^[A-Za-z0-9]{8,20}$"; 
+            // string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
             if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
