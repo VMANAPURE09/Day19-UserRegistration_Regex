9bc661ccb6ccfdf7ee68b58e0cb08299f17dac56
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-09 18:53:38 +0530

    UC8 Add One Special Character

--- UserResgistration/UserResgistration/Validation regex.cs
+++ UserResgistration/UserResgistration/Validation regex.cs
@@ -45,7 +45,8 @@ namespace UserResgistration
         {
             //string passwardpattern = "^[A-Za-z0-9]{8,20}$"; 
             // string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
-            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            //string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
             if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
@@ -53,3 +54,4 @@ namespace UserResgistration
         }
     }
 }
+ 
